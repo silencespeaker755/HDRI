2f8a61f7d73739ebca84e648ec51581af90e6510
silencespeaker <b06902019@ntu.edu.tw>
2022-03-22 02:03:00 +0800

    Add output parser

--- HDRI_generate.py
+++ HDRI_generate.py
@@ -4,10 +4,10 @@ import cv2
 from imageIO import save_HDR_images
 # from matplotlib import pyplot as plt
 
-def combine_BGR_files(B, G, R):
+def combine_BGR_files(B, G, R, output):
     HDR_image = cv2.merge([B, G, R])
     print(HDR_image)
-    save_HDR_images(HDR_image, "HDR.png")
+    save_HDR_images(HDR_image, output)
 
 
 if __name__ == "__main__":
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--blue", default="./HDR_B.npy")
     parser.add_argument("-g", "--green", default="./HDR_G.npy")
     parser.add_argument("-r", "--red", default="./HDR_R.npy")
+    parser.add_argument("-o", "--output", default="HDR.hdr")
     args = parser.parse_args()
 
     B = np.load(args.blue)
@@ -25,4 +26,4 @@ if __name__ == "__main__":
     G = np.exp(np.log2(G))
     R = np.exp(np.log2(R))
 
-    combine_BGR_files(B.reshape(3456, 4608), G.reshape(3456, 4608), R.reshape(3456, 4608))
+    combine_BGR_files(B.reshape(3456, 4608), G.reshape(3456, 4608), R.reshape(3456, 4608), args.output)
