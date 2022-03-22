7ab9161806cce0240c16598569952dbbb3f126ca
yukiyoru98 <33437532+yukiyoru98@users.noreply.github.com>
2022-03-23 02:09:14 +0800

    added robertson tonemapping, added HDRI_transform args

--- HDRI_transform.py
+++ HDRI_transform.py
@@ -5,12 +5,16 @@ from toneMapping import ToneMapping
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--blue"   , default="DebevecData/Radiance_B.npy")
-    parser.add_argument("-g", "--green"  , default="DebevecData/Radiance_G.npy")
-    parser.add_argument("-r", "--red"    , default="DebevecData/Radiance_R.npy")
-    parser.add_argument("-d", "--dir"    , default="DebevecData")
+    parser.add_argument("-b", "--blue"   , default="RobertsonData/tiger_final/Radiance_B.npy")
+    parser.add_argument("-g", "--green"  , default="RobertsonData/tiger_final/Radiance_G.npy")
+    parser.add_argument("-r", "--red"    , default="RobertsonData/tiger_final/Radiance_R.npy")
+    parser.add_argument("-d", "--dir"    , default="RobertsonData/tiger_final/")
     parser.add_argument("-m", "--method"    , default="global")
     parser.add_argument("-o", "--output" , default="LDR.png")
+    parser.add_argument("-a", "--a" , default=0.7)
+    parser.add_argument("-e", "--epsilon" , default=0.01)
+    parser.add_argument("-s", "--scale" , default=20)
+    parser.add_argument("-p", "--phi" , default=20)
     args = parser.parse_args()
 
     target_dir = args.dir
@@ -25,8 +29,8 @@ if __name__ == "__main__":
     # save hdr image
     output = args.output
     if method == "global":
-        ldr = ToneMapping.photographic_global(hdr, a=0.5)
+        ldr = ToneMapping.photographic_global(hdr, a=float(args.a))
         cv2.imwrite(os.path.join(target_dir, output), ldr)
     if method == "local":
-        ldr = ToneMapping.photographic_local(hdr, a=0.7, epsilon=0.01, scale_max=25, p=20.0)
+        ldr = ToneMapping.photographic_local(hdr, a=float(args.a), epsilon=float(args.epsilon), scale_max=int(args.scale), p=float(args.phi))
         cv2.imwrite(os.path.join(target_dir, output), ldr)
