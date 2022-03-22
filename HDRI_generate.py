21dafe1736fef82e925c556526869f288e5392ad
silencespeaker <b06902019@ntu.edu.tw>
2022-03-22 22:34:39 +0800

    Modifu parser argument input

--- HDRI_generate.py
+++ HDRI_generate.py
@@ -12,21 +12,21 @@ def combine_save_BGR_files(B, G, R, output):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--blue"   , default="./Inverse_CRF_B.npy")
-    parser.add_argument("-g", "--green"  , default="./Inverse_CRF_G.npy")
-    parser.add_argument("-r", "--red"    , default="./Inverse_CRF_R.npy")
+    parser.add_argument("-b", "--blue"   , default="./DebevecData/Radiance_B.npy")
+    parser.add_argument("-g", "--green"  , default="./DebevecData/Radiance_G.npy")
+    parser.add_argument("-r", "--red"    , default="./DebevecData/Radiance_R.npy")
     parser.add_argument("-d", "--dir"    , default="./DebevecData")
     parser.add_argument("-o", "--output" , default="HDR.hdr")
     args = parser.parse_args()
 
     target_dir = args.dir
-    B = np.load(os.path.join(target_dir, args.blue))
-    G = np.load(os.path.join(target_dir, args.green))
-    R = np.load(os.path.join(target_dir, args.red))
+    B = np.load(args.blue)
+    G = np.load(args.green)
+    R = np.load(args.red)
 
     # draw radiance map according to each color's data
     dict = {"blue": B, "green": G, "red": R}
-    draw_radiance_map(image_dict=dict, store_dir=args.dir)
+    draw_radiance_map(image_dict=dict, store_dir=target_dir)
 
     store_path = os.path.join(target_dir, args.output)
     combine_save_BGR_files(B, G, R, store_path)
--- HDRI_transform.py
+++ HDRI_transform.py
@@ -5,20 +5,20 @@ from toneMapping import ToneMapping
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--blue"   , default="./Inverse_CRF_B.npy")
-    parser.add_argument("-g", "--green"  , default="./Inverse_CRF_G.npy")
-    parser.add_argument("-r", "--red"    , default="./Inverse_CRF_R.npy")
+    parser.add_argument("-b", "--blue"   , default="./DebevecData/Radiance_B.npy")
+    parser.add_argument("-g", "--green"  , default="./DebevecData/Radiance_G.npy")
+    parser.add_argument("-r", "--red"    , default="./DebevecData/Radiance_R.npy")
     parser.add_argument("-d", "--dir"    , default="./DebevecData")
     parser.add_argument("-m", "--method"    , default="global")
-    parser.add_argument("-o", "--output" , default="HDR.hdr")
+    parser.add_argument("-o", "--output" , default="LDR.png")
     args = parser.parse_args()
 
     target_dir = args.dir
     method = args.method
 
-    B = np.load(os.path.join(target_dir, args.blue))
-    G = np.load(os.path.join(target_dir, args.green))
-    R = np.load(os.path.join(target_dir, args.red))
+    B = np.load(args.blue)
+    G = np.load(args.green)
+    R = np.load(args.red)
 
     hdr = cv2.merge([B, G, R])
 
